61a323e0338c83e8307a325b663dd0e08397dd72
Ronen Babayoff <ronen@lavaina.com>
2015-01-18 00:17:23 -0400

    Use futures to wait for child process async callbacks

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 var child_process = Npm.require("child_process");
+var Fiber = Npm.require("fibers");
+var Future = Npm.require('fibers/future');
 
 main = function() {
 
   console.log("I'm a meteor command line program!");
 
+  var future = new Future;
 
   var ls = child_process.spawn("ls");
 
   ls.stdout.setEncoding("utf8");
-  ls.stdout.on("data", function(){
-    console.log("ls.stdout.data")
+  ls.stdout.on("data", function(data){
+    console.log(data);
+    future.return(null);
   });
 
 
-  Meteor.setTimeout(function(){
-    console.log("timeout")
-  },100)
-
-
-
+  //Meteor.setTimeout(function(){
+  //  console.log("timeout");
+  //  future.return(null);
+  //}, 1000);
+  //
+  future.wait();
 };
