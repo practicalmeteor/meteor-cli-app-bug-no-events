119dc3a4421817f3e57b263c93e1276477f017c7
Ronen Babayoff <ronen@lavaina.com>
2015-01-20 01:44:28 -0400

    Leave example with only ls child process

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 var child_process = Npm.require("child_process");
 
 main = function() {
-
   console.log("I'm a meteor command line program!");
-
 
   var ls = child_process.spawn("ls");
 
   ls.stdout.setEncoding("utf8");
-  ls.stdout.on("data", function(){
-    console.log("ls.stdout.data")
+  ls.stdout.on("data", function(data){
+    process.stdout.write(data);
   });
-
-
-  Meteor.setTimeout(function(){
-    console.log("timeout")
-  },100)
-
-
-
+  ls.on('exit', function(){
+    console.log("ls on exit");
+    process.exit(0);
+  });
 };
